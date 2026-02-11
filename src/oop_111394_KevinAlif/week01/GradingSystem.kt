69edcf77b80274e710d243a543800f4b6c52b370
Kevin Alif <kevinalif57978@gmail.com>
2026-02-12 00:12:44 +0700

    week01: implement grading logic using when expression

--- src/oop_111394_KevinAlif/week01/GradingSystem.kt
+++ src/oop_111394_KevinAlif/week01/GradingSystem.kt
@@ -1,8 +1,18 @@
 package oop_111394_KevinAlif.week01
 
 fun main() {
+    // LANGKAH 2: Refactoring Variables (Checkpoint 2)
     val name = "John Thor"
     val score = 80
-
     println("Nama: $name, Nilai: $score")
+
+    // LANGKAH 3: When Expression (Checkpoint 3)
+    // Logika ini harus ada DI DALAM kurung kurawal main
+    val grade = when (score) {
+        in 90..100 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        else -> "D"
+    }
+    println("Grade kamu: $grade")
 }
