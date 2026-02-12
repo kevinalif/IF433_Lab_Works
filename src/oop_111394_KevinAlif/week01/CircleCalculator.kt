3bbd6a31fc53ae4778dc0367cf981c82cc27b1cd
Kevin Alif <kevinalif57978@gmail.com>
2026-02-12 23:08:53 +0700

    week01: (tugas 1) use expression body for checkSize function

--- src/oop_111394_KevinAlif/week01/CircleCalculator.kt
+++ src/oop_111394_KevinAlif/week01/CircleCalculator.kt
@@ -1,24 +1,17 @@
 package oop_111394_KevinAlif.week01
 
-fun main (args: Array<String>) {
-    // Variable definition
-    var radius: Double = 7.0
-    var pi: Double = 3.14
+fun main(args: Array<String>) {
+    val radius = 7.0
+    val pi = 3.14
+    val area = pi * radius * radius
 
-    // Calculation
-    var area: Double = pi * radius * radius
-
-    // Output Concatenation
-    println("Radius: " + radius + ", Area: " + area)
-
-    // Logic check
+    println("Radius: $radius, Area: $area")
     checkSize(area)
 }
 
-fun checkSize (area: Double) {
-    if (area > 100) {
-        println("This is a Big Circle")
-    } else {
-        println("This is a Small Circle")
-    }
+// CHECKPOINT 8 & 9: Pakai '=' (Expression Body) dan pastikan cuma ADA SATU
+fun checkSize(area: Double) = if (area > 100) {
+    println("This is a Big Circle")
+} else {
+    println("This is a Small Circle")
 }
--- src/oop_111394_KevinAlif/week01/GradingSystem.kt
+++ src/oop_111394_KevinAlif/week01/GradingSystem.kt
@@ -13,11 +13,3 @@ fun main (args: Array<String>) {
 
     checkSize(area)
 }
-
-fun checkSize (area: Double) {
-    if (area > 100) {
-        println("This is a Big Circle")
-    } else {
-        println("This is a Small Circle")
-    }
-}
