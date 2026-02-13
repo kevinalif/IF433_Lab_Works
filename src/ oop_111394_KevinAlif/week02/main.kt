e5e24cc89f01a643e43d6ae8a5fdec959ee82721
Kevin Alif <kevinalif57978@gmail.com>
2026-02-13 23:35:03 +0700

    week02: implement selection logic for constructors

--- src/ oop_111394_KevinAlif/week02/main.kt
+++ src/ oop_111394_KevinAlif/week02/main.kt
@@ -4,17 +4,12 @@ import java.util.Scanner
 
 fun main() {
     val scanner = Scanner(System.`in`)
-
     println("---APLIKASI PMB UMN ---")
-
     print("Masukkan Nama: ")
     val name = scanner.nextLine()
-
     print("masukkkan NIM (Wajib 5 Karakter): ")
     val nim = scanner.next()
-
     scanner.nextLine()
-
     if (nim.length != 5) {
         println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
     } else {
@@ -22,23 +17,16 @@ fun main() {
         println("1. Reguler (Isi Jurusan)")
         println("2. Umum (Jurusan Otomatis 'Non-Matriculated')")
         print("Pilihan Anda (1/2): ")
-
         val type = scanner.nextInt()
         scanner.nextLine()
-
         if (type == 1) {
-            // JALUR 1: Input manual
             print("Masukkan Jurusan: ")
             val major = scanner.nextLine()
-
             val s1 = Student(name, nim, major)
             println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
-
         } else if (type == 2) {
-            // JALUR 2: Pakai Default Argument
             val s2 = Student(name, nim)
             println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
-
         } else {
             println("Pilihan ngawur, pendaftaran batal!")
         }
