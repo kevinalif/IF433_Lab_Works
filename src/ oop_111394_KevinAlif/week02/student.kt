dba8eeea2348189e2261cf7009f5f7a86ead8786
Kevin Alif <kevinalif57978@gmail.com>
2026-02-13 10:33:13 +0700

    week02: add warning validation in init block

--- src/ oop_111394_KevinAlif/week02/student.kt
+++ src/ oop_111394_KevinAlif/week02/student.kt
@@ -5,5 +5,14 @@ class student (
     val nim: String,
     val major: String
 ){
-
+class student_(val name: String, val nim: String, val major: String) {
+    init {
+        if (nim.length !=5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("data mahasiswa $name mungkin akan bermasalah di sistem. ")
+        } else {
+            println("LOG: Objek student $name berhasil dialokasikan di Memory. ")
+        }
+    }
+}
 }
