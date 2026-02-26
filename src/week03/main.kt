781467f703d644272b7f3fce784a218c0b4f3802
Kevin Alif <kevinalif57978@gmail.com>
2026-02-26 21:15:01 +0700

    week03: (task 1) implement Weapon testing in main

--- src/week03/main.kt
+++ src/week03/main.kt
@@ -10,4 +10,12 @@ fun main() {
     e.printStatus()
 
     println("Pajak yang harus dibayar: ${e.tax}")
+
+    println("\n--- Pengujian Weapon ---")
+    val sword = Weapon("Excalibur")
+
+    sword.damage = -50
+    sword.damage = 9999
+
+    println("Senjata: ${sword.name}, Damage: ${sword.damage}, Tier: ${sword.tier}")
 }
