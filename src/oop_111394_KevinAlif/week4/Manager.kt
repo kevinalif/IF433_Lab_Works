fea7b95ea89ddebc7dafabd272d0760e507173cc
Kevin Alif <kevinalif57978@gmail.com>
2026-02-28 02:23:22 +0700

    week04: (task 2) create Manager class overriding Employee

---
+++ src/oop_111394_KevinAlif/week4/Manager.kt
@@ -0,0 +1,12 @@
+package oop_111394_KevinAlif.week4
+
+class Manager(name: String, baseSalary: Int) : Employee(name, baseSalary) {
+
+    override fun work() {
+        println("$name sedang memimpin rapat divisi.")
+    }
+
+    override fun calculateBonus(): Int {
+        return super.calculateBonus() + 500000
+    }
+}
