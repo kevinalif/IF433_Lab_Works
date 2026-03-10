286273e30f9ec4ecd46a4113056353d181af2fb3
Kevin Alif <kevinalif57978@gmail.com>
2026-03-10 23:46:51 +0700

    week05: implement polymorphic collection and smart casting

---
+++ src/oop_111394_KevinAlif/Week5/Main.kt
@@ -0,0 +1,25 @@
+package oop_111394_KevinAlif.Week5
+
+fun main() {
+    val dosen1 = Dosen(nama = "Pak Alex", nidn = "0123456")
+    val admin1 = Admin(nama = "Bu Siti")
+
+    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+
+    println("=== AKTIVITAS PEGAWAI ===")
+    for (pegawai in daftarPegawai) {
+        pegawai.bekerja()
+
+        when (pegawai) {
+            is Dosen -> {
+                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
+                pegawai.mengajar()
+            }
+            is Admin -> {
+                println("=> Terdeteksi sebagai Admin")
+                pegawai.doAdminWork()
+            }
+        }
+        println()
+    }
+}
