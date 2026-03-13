d611460a44949f3e413bfdb5556c8c9832e7ee26
Kevin Alif <kevinalif57978@gmail.com>
2026-03-13 14:42:01 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_111394_KevinAlif/Week6/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_111394_KevinAlif.Week6
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.") }
+}
