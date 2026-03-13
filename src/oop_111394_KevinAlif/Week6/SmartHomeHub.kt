976566c144d6ddcbd5f2808e36d2cb352ee49a21
Kevin Alif <kevinalif57978@gmail.com>
2026-03-13 16:00:48 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_111394_KevinAlif/Week6/SmartHomeHub.kt
@@ -0,0 +1,17 @@
+package oop_111394_KevinAlif.Week6
+
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
