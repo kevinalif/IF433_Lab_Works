13bd858a7af56d1502b02da651561692329c61e2
Kevin Alif <kevinalif57978@gmail.com>
2026-04-04 02:14:15 +0700

    week07: (task) create GameItem data class

--- src/oop_111394_KevinAlif/Week7/GameModels.kt
+++ src/oop_111394_KevinAlif/Week7/GameModels.kt
@@ -3,3 +3,4 @@ package oop_111394_KevinAlif.Week7
 enum class ItemRarity(val dropChance: Int) {
     COMMON(70), UNCOMMON(50), RARE(20), EPIC(10), LEGENDARY(1)
 }
+data class GameItem(val name: String, val damage: Int, val rarity: ItemRarity)
