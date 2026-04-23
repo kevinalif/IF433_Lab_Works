61c2bddbc83b11c585f8d9928a9c7bf12868148e
Kevin Alif <kevinalif57978@gmail.com>
2026-04-23 21:27:11 +0700

    week08: create fallback logic with as? and elvis

--- src/oop_111394_KevinAlif/Week8/Main.kt
+++ src/oop_111394_KevinAlif/Week8/Main.kt
@@ -31,3 +31,7 @@ for (item in mixedData) {
         println("Ditemukan teks: ${it.uppercase()}")
     }
 }
+
+val someObject: Any = 100
+val safeString = someObject as? String ?: "Unknown String"
+println("Hasil cast fallback: $safeString")
