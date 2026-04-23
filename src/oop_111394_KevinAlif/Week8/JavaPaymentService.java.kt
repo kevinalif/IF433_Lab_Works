84f91266e19a64edfaa102f8385ab97cdee5c649
Kevin Alif <kevinalif57978@gmail.com>
2026-04-23 23:06:48 +0700

    week08: (task) implement payment processing with !!

--- src/oop_111394_KevinAlif/Week8/JavaPaymentService.java.kt
+++ src/oop_111394_KevinAlif/Week8/JavaPaymentService.java.kt
@@ -5,3 +5,11 @@ object JavaPaymentService {
         return "TRX-" + productId + "-SUCCESS"
     }
 }
+fun checkout(product: Product) {
+    val id = when (product) {
+        is Electronic -> product.id
+        is Clothing -> product.id
+    }
+    val transactionResult = JavaPaymentService.processPayment(id)
+    println(transactionResult!!)
+}
