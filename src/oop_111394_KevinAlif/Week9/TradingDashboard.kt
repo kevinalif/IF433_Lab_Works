2278820842d46ffd09319a2b40a5d9f645d0fa46
Kevin Alif <kevinalif57978@gmail.com>
2026-04-24 14:43:17 +0700

    week09: (task) print worst performers and unique pairs

--- src/oop_111394_KevinAlif/Week9/TradingDashboard.kt
+++ src/oop_111394_KevinAlif/Week9/TradingDashboard.kt
@@ -28,3 +28,6 @@ fun main() {
 
     println("=== CRYPTO TRADING DASHBOARD ===")
     topPerformersString.forEach { println(it) }
+
+    worstPerformersString.forEach { println(it) }
+    println("\nUnique Pairs: $uniquePairs")
