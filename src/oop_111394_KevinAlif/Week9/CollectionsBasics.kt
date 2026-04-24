7e7e96b40ffed13c585b8262f839594a61ec788e
Kevin Alif <kevinalif57978@gmail.com>
2026-04-24 13:56:02 +0700

    week09: implement map for key-value storage

--- src/oop_111394_KevinAlif/Week9/CollectionsBasics.kt
+++ src/oop_111394_KevinAlif/Week9/CollectionsBasics.kt
@@ -7,16 +7,30 @@ fun main() {
     val frameworks: List<String> = listOf("Kotlin", "Java", "C++")
     println("Immutable List: $frameworks")
 
-    val scores : MutableList<Int> = mutableListOf(85, 90)
+    val scores: MutableList<Int> = mutableListOf(85, 90)
     scores.add(95)
     scores[0] = 88
 
     println("\n === TEST SET ===")
-    val  uniqueNumbers = setOf(1, 2, 2, 3, 3, 4)
+    val uniqueNumbers = setOf(1, 2, 2, 3, 3, 4)
     println("unique Numbers (Set): $uniqueNumbers")
 
     val activeUsers = mutableSetOf("UserA", "UserB")
     activeUsers.add("UserC")
     activeUsers.add("UserA")
     println("Active Users: $activeUsers")
+
+    println("\n === TEST MAP ===")
+    val studentGrades = mapOf(
+        "Alice" to "A",
+        "Bob" to "B",
+        "Charlie" to "A"
+    )
+    println("Nilai Bob: ${studentGrades["Bob"]}")
+
+    val inventory = mutableMapOf("Apples" to 50, "Bananas" to 30)
+    inventory["Oranges"] = 20
+    inventory["Apples"] = 45
+    println("Inventory: $inventory")
 }
+
