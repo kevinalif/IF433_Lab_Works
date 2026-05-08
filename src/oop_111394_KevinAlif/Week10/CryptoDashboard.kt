2a3adcd35d9b380188510f022dc29bd73b3677ce
Kevin Alif <kevinalif57978@gmail.com>
2026-05-08 22:03:43 +0700

    week10: (task) populate coin repository

--- src/oop_111394_KevinAlif/Week10/CryptoDashboard.kt
+++ src/oop_111394_KevinAlif/Week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_111394_KevinAlif.Week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 4.2))
+    coinRepo.add(Coin("USDT", 1500.0))
 }
