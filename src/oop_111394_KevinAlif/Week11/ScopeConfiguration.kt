ec33d43796e03942f8742c34ba6de8fa18c9b211
Kevin Alif <kevinalif57978@gmail.com>
2026-05-14 20:46:43 +0700

    week11: implement with function for multiple operations

--- src/oop_111394_KevinAlif/Week11/ScopeConfiguration.kt
+++ src/oop_111394_KevinAlif/Week11/ScopeConfiguration.kt
@@ -9,10 +9,16 @@ fun main() {
         age = 25
     }
     println(user)
+
     println("\n=== TEST ALSO ===")
     val numbers = mutableListOf(1, 2, 3)
     numbers.also {
         println("Log Sebelum ditambah: $it")
     }.add(4)
     println("Setelah ditambah: $numbers")
+
+    println("\n=== TEST WITH ===")
+    with(user) {
+        println("User Detail -> Nama: $name, Umur: $age")
+    }
     }
