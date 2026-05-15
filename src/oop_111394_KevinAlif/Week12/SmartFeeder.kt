1d57ea7fa840ba3be196519d5b93085e20ee8b4a
Kevin Alif <kevinalif57978@gmail.com>
2026-05-15 10:42:05 +0700

    week12: (task) handle feeding success using onSuccess

--- src/oop_111394_KevinAlif/Week12/SmartFeeder.kt
+++ src/oop_111394_KevinAlif/Week12/SmartFeeder.kt
@@ -51,4 +51,11 @@ fun main() {
     runCatching {
         dispenseKibble(requestedGram = 30, availableGram = 1000, isJammed = false)
     }
+    runCatching {
+        dispenseKibble(requestedGram = 30, availableGram = 1000, isJammed = false)
+    }.onSuccess { newStock ->
+        currentKibbleStock = newStock
+        println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr")
+    }
 }
+
