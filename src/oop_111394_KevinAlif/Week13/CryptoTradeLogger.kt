9868a036bcbf481ca3c01b9c5e37a0e3c1f12f94
Kevin Alif <kevinalif57978@gmail.com>
2026-05-22 23:19:13 +0700

    week13: (task) implement toCsv for TradeRecord

--- src/oop_111394_KevinAlif/Week13/CryptoTradeLogger.kt
+++ src/oop_111394_KevinAlif/Week13/CryptoTradeLogger.kt
@@ -3,3 +3,7 @@ import java.io.File
 import java.io.FileNotFoundException
 
 data class TradeRecord(val id: Int, val symbol: String, val type: String, val margin: Double, val pnl: Double)
+
+fun TradeRecord.toCsv(): String {
+    return "$id,$symbol,$type,$margin,$pnl"
+}
