06d1acbec0bfef92d79ba0759511259161d13811
Kevin Alif <kevinalif57978@gmail.com>
2026-05-22 23:21:56 +0700

    week13: (task) test full crypto trade logger pipeline

--- src/oop_111394_KevinAlif/Week13/CryptoTradeLogger.kt
+++ src/oop_111394_KevinAlif/Week13/CryptoTradeLogger.kt
@@ -48,3 +48,6 @@ fun main() {
     File("crypto_trades.csv").appendText("CORRUPT_ID, DOGEUSDT, Hold, XX,YY\n")
     val loadedData = loadTrades("crypto_trades.csv")
     val totalPnl = loadedData.sumOf { it.pnl }
+    loadedData.forEach { println(it) }
+    println("==== TOTAL PnL BERSIH: $$totalPnl ====")
+}
