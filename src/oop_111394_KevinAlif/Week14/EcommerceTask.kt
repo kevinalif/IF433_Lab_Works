86c7cebfe9757d436eba128868ba5bc1a2cd3dc9
Kevin Alif <kevinalif57978@gmail.com>
2026-06-08 18:46:02 +0700

    week14: (task) copy bad E-Commerce Order Processor code

---
+++ src/oop_111394_KevinAlif/Week14/EcommerceTask.kt
@@ -0,0 +1,19 @@
+package oop_111394_KevinAlif.Week14
+
+import java.io.File
+
+class BadOrderProcessor {
+    private val file = File("orders.csv")
+
+    fun processOrder(itemName: String, basePrice: Double, customerType: String) {
+        val finalPrice = when (customerType) {
+            "REGULAR" -> basePrice
+            "VIP" -> basePrice * 0.90 // Diskon 10%
+            else -> basePrice
+        }
+
+        println("Memproses pesanan $itemName seharga $finalPrice")
+        file.appendText("$itemName, $finalPrice, $customerType\n")
+        println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
+    }
+}
